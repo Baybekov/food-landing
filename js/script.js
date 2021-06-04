c6ff678220ca843c0f676eafb76ef7aa4129bc7d
Oleg Baybekov <baybekov.oleg@gmail.com>
2021-06-04 16:23:42 +0300

    fetch added

--- js/script.js
+++ js/script.js
@@ -240,10 +240,10 @@ window.addEventListener('DOMContentLoaded', () => {
             `;
             form.insertAdjacentElement('afterend', statusMessage);
 
-            const request = new XMLHttpRequest();
-            request.open('POST', 'server.php');
+            // const request = new XMLHttpRequest();
+            // request.open('POST', 'server.php');
 
-            request.setRequestHeader('Content-type', 'application/json');
+
             const formData = new FormData(form);
 
             const object = {};
@@ -252,22 +252,27 @@ window.addEventListener('DOMContentLoaded', () => {
 
             });
 
-            const json = JSON.stringify(object);
 
+            fetch('server.php', {
+                method: "POST",
+                headers: {
+                    'Content-type': 'application/json'
+                },
+                body: JSON.stringify(object)
 
-            request.send(json);
-
-            request.addEventListener('load', () => {
-                if (request.status === 200) {
-                    console.log(request.response);
-                    showThanksModal(message.success);
-                    form.reset();
-                    statusMessage.remove();
-                } else {
-                    showThanksModal(message.failure);
-                }
+            })
+            .then(data => data.text())
+            .then(data => {
+                console.log(data);
+                showThanksModal(message.success);
+                statusMessage.remove();
+            })
+            .catch(() => {
+                showThanksModal(message.failure);
+            })
+            .finally(() => {
+                form.reset();
             });
-
         });
     }
 
