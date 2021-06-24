bb392e4f75d77f2266d5c71e66ba33c3d469e11a
Oleg Baybekov <baybekov.oleg@gmail.com>
2021-06-24 15:32:23 +0300

    babel required

--- js/script.js
+++ js/script.js
@@ -1,3 +1,7 @@
+require('es6-promise').polyfill();
+
+import 'nodelist-foreach-polyfill';
+
 import tabs from './modules/tabs';
 import modal from './modules/modal';
 import timer from './modules/timer';
@@ -12,7 +16,7 @@ window.addEventListener('DOMContentLoaded', function() {
 
     tabs('.tabheader__item', '.tabcontent', '.tabheader__items', 'tabheader__item_active');
     modal('[data-modal]', '.modal', modalTimerId);
-    timer('.timer', '2020-06-11');
+    timer('.timer', '2022-06-11');
     cards();
     calc();
     forms('form', modalTimerId);
